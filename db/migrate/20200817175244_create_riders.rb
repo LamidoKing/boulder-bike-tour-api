4c2daea650409c87e7fcfaa5e6b7f94cbeb11ee7
lamido <kinglamido@gmail.com>
2020-08-19 23:07:18 +0100

    ft:add not null to colunms in  create_riders.rb migration file

--- db/migrate/20200817175244_create_riders.rb
+++ db/migrate/20200817175244_create_riders.rb
@@ -1,12 +1,12 @@
 class CreateRiders < ActiveRecord::Migration[6.0]
   def change
     create_table :riders do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :city_of_origin
-      t.string :state_of_origin
-      t.decimal :latitude, { precision: 10, scale: 6 }
-      t.decimal :longitude, { precision: 10, scale: 6 }
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :city_of_origin, null: false
+      t.string :state_of_origin, null: false
+      t.decimal :latitude, { precision: 10, scale: 6 }, null: false
+      t.decimal :longitude, { precision: 10, scale: 6 }, null: false
       t.belongs_to :admin, null: false, foreign_key: true
 
       t.timestamps
