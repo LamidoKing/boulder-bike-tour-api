8fa841cb87c65ef470fa728179f191ce6f0854db
lamido <kinglamido@gmail.com>
2020-08-19 12:26:39 +0100

    ft:add some validations to submission

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,6 @@
 class Submission < ApplicationRecord
+    validates :slogan, length: { maximum: 50 }, presence: true
+    EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, format: { with: EMAIL_FORMAT }, uniqueness: true
+    validates :first_name, :last_name, presence: true
 end
--- test/models/submission_test.rb
+++ test/models/submission_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class SubmissionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should have slogan not morethan 50 characters" do
+    submission = submissions(:one)
+    submission.slogan = "Validations are an important part when building any kind of application."
+    assert_not submission.valid?
+  end
+
+  test "should have first_name" do
+    submission = submissions(:one)
+    submission.first_name = nil
+    assert_not submission.valid?
+  end
+
+  test "should have last_name" do
+    submission = submissions(:one)
+    submission.last_name = nil
+    assert_not submission.valid?
+  end
+  
+  test "should have valid email" do
+    submission = submissions(:one)
+    submission.email = nil
+    assert_not submission.valid?
+  end
+
 end
